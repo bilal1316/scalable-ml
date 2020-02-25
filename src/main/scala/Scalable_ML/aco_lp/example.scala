df09a8b1925c428e155774e2e4e2216e450fb3a1
Muhammad Bilal <muhammad.billall13@gmail.com>
2020-02-25 11:30:16 +0500

    Initial Commit

---
+++ src/main/scala/Scalable_ML/aco_lp/example.scala
@@ -0,0 +1,7 @@
+package Scalable_ML
+
+package object aco_lp {
+  def main(args : Array[String]) {
+    println( "Hello Ant Colony!" )
+  }
+}
---
+++ src/main/scala/Scalable_ML/nProximity/example.scala
@@ -0,0 +1,11 @@
+package Scalable_ML.nProximity
+import org.apache.spark._
+/**
+ * @author ${user.name}
+ */
+
+object nProximity {
+  def main(args : Array[String]) {
+    println( "Hello nProximity!" )
+  }
+}
